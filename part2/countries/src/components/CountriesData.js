db51d8e75ab9b6987546e882e4d40eeb4c1a06d9
Vitiligo <88615893+vitiligo610@users.noreply.github.com>
2023-04-02 09:23:15 +0500

    Update CountriesData.js

--- part2/countries/src/components/CountriesData.js
+++ part2/countries/src/components/CountriesData.js
@@ -2,8 +2,58 @@ import { useState, useEffect } from "react"
 import axios from 'axios'
 
 function CountriesData ({ data, single }) {
-    data.name.common == 'Palestine' && console.log(data)
+    // data.name.common == 'Palestine' && console.log(data)
     const [visible, setVisible] = useState(false)
+    const [coordinates, setCoordinates] = useState({
+        lat: null,
+        lon: null
+    })
+    const [weatherData, setWeatherData] = useState(null)
+    const [loadingWeather, setLoadingWeather] = useState(true)
+
+    const appId = 'e012c27ed355e7aa7790ae554ce61d93'
+    const cityName = data.capital[0]
+
+    const geoDataUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${appId}`
+
+
+    useEffect(() => {
+        axios
+            .get(geoDataUrl)
+            .then(response => {
+                setCoordinates({
+                    lat: response.data[0].lat,
+                    lon: response.data[0].lon
+                })
+            })
+    }, [])
+        
+    // data.name.common == 'Palestine' && console.log(coordinates)
+    
+    
+    
+    useEffect(() => {
+        if (coordinates.lat !== null && coordinates.lon !== null){
+            const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=hourly,daily&units=metric&appid=${appId}`
+        
+        axios
+            .get(url)
+            .then(response => {
+                setWeatherData(
+                    {
+                        temp: response.data.current.temp,
+                        description: response.data.current.weather[0].description,
+                        icon: response.data.current.weather[0].icon,
+                        wind_speed: response.data.current.wind_speed
+                    }
+                )
+                setLoadingWeather(!loadingWeather)
+            })
+        }
+    }, [coordinates])
+    // data.name.common === 'Palestine' && console.log(weatherData)
+
+
 
     function loopLanguages (languages) {
         const values = []
@@ -56,8 +106,18 @@ function CountriesData ({ data, single }) {
                     <b>Capital:</b> {loopCapitals(data.capital)}<br />
                     <b>Area:</b> {data.area} km²<br /><br />
                     <b>Languages:</b> <ul>{loopLanguages(data.languages)}</ul>
-                    <img src={data.flags.png} /><br /><br />
-                    {data.flags.alt && <><b>Flag Alt:</b> {data.flags.alt}</>}
+                    <img className="flag" src={data.flags.png} /><br /><br />
+                    {data.flags.alt && <><b>Flag Alt:</b> {data.flags.alt}</>}<br />
+                    {loadingWeather && <h2>Loading weather data...</h2>}
+                    {
+                        weatherData !== null &&
+                        <div className="weather">
+                            <h2>Weather in {data.capital[0]}</h2>
+                            <b>Temperature:</b> {weatherData.temp} °C<br />
+                            <img src={`https://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} alt={weatherData.description} /><br />
+                            <b>Wind Speed:</b> {weatherData.wind_speed} ms⁻¹<br /><br />
+                        </div>
+                    }
                     <div className="border"></div>
                 </>
             }
