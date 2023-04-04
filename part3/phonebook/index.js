438689c4bbb32b37f7432f5a566fc53f8c545083
Vitiligo <88615893+vitiligo610@users.noreply.github.com>
2023-04-04 13:20:58 +0500

    Create index.js

---
+++ part3/phonebook/index.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
+    },
+    { 
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
+    },
+    { 
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Phonebook App</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people.<br /> ${new Date()}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(n => n.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+const generateId = () => Math.floor(Math.random() * 1000000)
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const names = [...persons].map(person => person.name)
+
+    if (!body.name || !body.number) {
+        return response.json({
+            error: "Name or phone number is missing"
+        })
+    }
+
+    if (names.includes(body.name)) {
+        return response.json({
+            error: "Name must be unique"
+        })
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log('Server is up and running')
+})
