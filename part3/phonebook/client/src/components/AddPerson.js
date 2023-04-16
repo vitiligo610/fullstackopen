9f5f148c61cd00ef6009bb592e55213535523401
Vitiligo <88615893+vitiligo610@users.noreply.github.com>
2023-04-16 17:57:42 +0500

    Update AddPerson.js

--- part3/phonebook/client/src/components/AddPerson.js
+++ part3/phonebook/client/src/components/AddPerson.js
@@ -1,13 +1,18 @@
-const AddPerson = ({ addPerson, newName, handleNameChange, newPhone, handlePhoneChange }) => {
+const AddPerson = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
     return (
         <>
             <h2>add a new</h2>
             <form onSubmit={addPerson}>
-                <div>
+                <div className="name">
                 name: <input value={newName} onChange={handleNameChange} />
                 </div>
-                <div>
-                phone: <input value={newPhone} onChange={handlePhoneChange} />
+                <div className="wrapper">
+                    <div className="phone">
+                    phone: <input value={newNumber} onChange={handleNumberChange} />
+                    </div>
+                    <div className="format">
+                        <pre>format: 09-1234556 or 040-22334455</pre>
+                    </div>
                 </div>
                 <div>
                 <button type="submit">add</button>
