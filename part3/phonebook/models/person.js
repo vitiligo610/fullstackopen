6bf68c38b37a73070bdad089e5170cfd027627d9
Vitiligo <88615893+vitiligo610@users.noreply.github.com>
2023-04-16 17:46:40 +0500

    Update person.js

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -15,8 +15,16 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    match: /^\d{2,3}-\d{7,8}$/
+  }
 })
 
 personSchema.set('toJSON', {
