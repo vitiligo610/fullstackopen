c9c94e1f857476d998b1e1ffc0bbeef8fb4a3adb
Vitiligo <88615893+vitiligo610@users.noreply.github.com>
2023-04-30 16:03:17 +0500

    Delete part4/bloglist directory

--- part4/bloglist/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
