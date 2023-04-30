ebb76ac6e8c32bdbe46a1aa4e9c6df71e7116202
Vitiligo <88615893+vitiligo610@users.noreply.github.com>
2023-04-30 16:04:03 +0500

    Add files via upload

---
+++ part4/bloglist/app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+require('express-async-errors')
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connection to', process.env.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
+
+app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
