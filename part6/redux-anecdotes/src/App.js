24b1173e416a1447fda6e9e07e304d29459cd2a5
Ahmed Javed <softwareguild2@gmail.com>
2023-09-05 11:14:02 +0000

    Complete ex 6.3-6.6

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -6,12 +7,22 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
+    dispatch(voteAnecdote(id))
   }
+
+  const add = e => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+  }
+
+  const byVotes = (v1, v2) => v2.votes - v1.votes
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.sort(byVotes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -22,9 +33,10 @@ const App = () => {
           </div>
         </div>
       )}
+
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={add}>
+        <div><input name="anecdote" /></div>
         <button>create</button>
       </form>
     </div>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -22,8 +22,40 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch(action.type) {
+    case 'ADD_ANECDOTE':
+      return [...state, action.payload]
+    case 'VOTE':
+      const id = action.payload.id
+      const anecdoteToChange = state.find(anecdote => anecdote.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote => anecdote.id !== id ? anecdote : changedAnecdote)
+    default:
+      return state
+  }
+}
 
-  return state
+export const createAnecdote = (content) => {
+  return {
+    type: 'ADD_ANECDOTE',
+    payload: {
+      content,
+      id: getId(),
+      votes: 0
+    }
+  }
+}
+
+export const voteAnecdote = id => {
+  return {
+    type: 'VOTE',
+    payload: {
+      id
+    }
+  }
 }
 
 export default reducer
