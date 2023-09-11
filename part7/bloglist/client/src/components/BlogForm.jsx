5bea3876320e758cc99f00bbf1815fbab2d8b305
Ahmed Javed <softwareguild2@gmail.com>
2023-09-11 19:52:47 +0000

    Copy bloglist client from part5 and convert cra to vite

---
+++ part7/bloglist/client/src/components/BlogForm.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+  const [newBlog, setNewBlog] = useState({
+    title: '',
+    author: '',
+    url: ''
+  })
+
+  const addBlog = e => {
+    e.preventDefault()
+    createBlog({
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url
+    })
+  }
+
+  return (
+    <form onSubmit={addBlog}>
+      <h2>create new</h2>
+      <div>
+        title: <input
+          type='text'
+          value={newBlog.title}
+          name='Title'
+          onChange={({ target }) => setNewBlog(prev => ({ ...prev, title: target.value }))}
+        />
+      </div>
+      <div>
+        author: <input
+          type='text'
+          value={newBlog.author}
+          name='Author'
+          onChange={({ target }) => setNewBlog(prev => ({ ...prev, author: target.value }))}
+        />
+      </div>
+      <div>
+        url: <input
+          type='text'
+          value={newBlog.url}
+          name='Url'
+          onChange={({ target }) => setNewBlog(prev => ({ ...prev, url: target.value }))}
+        />
+      </div>
+      <button>create</button>
+    </form>
+  )
+}
+
+export default BlogForm
