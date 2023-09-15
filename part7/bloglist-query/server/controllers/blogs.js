6299afd3df0341375f1a077caccd8bb679857471
Ahmed Javed <softwareguild2@gmail.com>
2023-09-15 18:49:31 +0000

    Create new branch & clone bloglist dir

---
+++ part7/bloglist-query/server/controllers/blogs.js
@@ -0,0 +1,91 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const logger = require('../utils/logger')
+
+const jwt = require('jsonwebtoken')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const id = request.params.id
+  const blog = await Blog.findById(id)
+  if (blog) {
+    response.status(200).json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', async (request, response) => {
+  let { title, author, url, likes, userId } = request.body
+  
+  if (!title || !author || !url) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  if (!likes) {
+    likes = 0
+  }
+
+  const token = request.token
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+
+  const blog = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes,
+    user: user.id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const id = request.params.id
+
+  const blog = {
+    title: title,
+    author: author,
+    url: url,
+    likes: likes
+  }
+
+  const updatedNote = await Blog.findByIdAndUpdate(id, blog, { new: true })
+  response.status(200).json(updatedNote)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  const token = request.token
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
+  const blogToDelete = await Blog.findById(id)
+
+  if (blogToDelete.user._id.toString() === user._id.toString()) {
+    await Blog.findByIdAndRemove(id)
+    response.status(204).end()
+  } else {
+    response.status(401).json({ error: 'unauthorized action' })
+  }
+})
+
+module.exports = blogsRouter
---
+++ part7/bloglist-query/server/controllers/users.js
@@ -0,0 +1,37 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+    .find({})
+    .populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+
+  response.status(200).json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  if (!password) {
+    return response.status(400).json({ error: 'password cannot be left empty' })
+  }
+
+  if (password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save().catch(error => response.status(400).json({ error: error.message }))
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
