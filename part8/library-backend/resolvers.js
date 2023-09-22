9f413e09af2de346106b132bddec5751ab298cf9
Ahmed Javed <softwareguild2@gmail.com>
2023-09-22 11:11:03 +0000

    Move typeDefs and resolvers to their own files

---
+++ part8/library-backend/resolvers.js
@@ -0,0 +1,152 @@
+const { GraphQLError } = require('graphql')
+const jwt = require('jsonwebtoken')
+
+const Book = require('./models/book')
+const Author = require('./models/author')
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      const author = args.author
+      const genre = args.genre
+      if (author) {
+        const author = await Author.find({ name: author })
+        if (genre) {
+          return Book.find({ author, genres: { $in: [genre] } }).populate(
+            'author'
+          )
+        }
+        return Book.find({ author }).populate('author')
+      }
+      if (genre) {
+        return Book.find({ genres: { $in: [genre] } }).populate('author')
+      }
+      return Book.find({}).populate('author')
+    },
+    allAuthors: async () => {
+      return Author.find({})
+    },
+    me: async (root, args, context) => {
+      return context.currentUser
+    },
+    allGenres: async () => {
+      const books = await Book.find({})
+      const genresWithDuplicates = books.map((b) => b.genres.map((g) => g)).flat()
+      return [...new Set(genresWithDuplicates)]
+    }
+  },
+  Author: {
+    bookCount: async (root) => {
+      const author = await Author.find({ name: root.name })
+      const booksByAuthor = await Book.find({ author })
+      return booksByAuthor.length
+    }
+  },
+  Mutation: {
+    addBook: async (root, args, context) => {
+      const currentUser = context.currentUser
+
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const foundAuthor = await Author.findOne({ name: args.author })
+
+      if (!foundAuthor) {
+        const author = new Author({ name: args.author })
+        try {
+          await author.save()
+        } catch (error) {
+          throw new GraphQLError(
+            'Author name should be atleast 4 characters long',
+            {
+              extensions: {
+                code: 'BAD_USER_INPUT',
+                invalidArgs: args.author,
+                error
+              }
+            }
+          )
+        }
+      }
+
+      const author = await Author.findOne({ name: args.author })
+      const book = new Book({ ...args, author })
+
+      try {
+        await book.save()
+      } catch (error) {
+        throw new GraphQLError(
+          'Book title should be atleast 5 characters long',
+          {
+            extensions: {
+              code: 'BAD_USER_INPUT',
+              invalidArgs: args.title,
+              error
+            }
+          }
+        )
+      }
+
+      return book
+    },
+    editAuthor: async (root, args, context) => {
+      const currentUser = context.currentUser
+
+      if (!currentUser) {
+        throw new GraphQLError('not authenticated', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const name = args.name
+      const author = await Author.findOne({ name })
+      if (!name && !author) {
+        return null
+      }
+      await Author.findOneAndUpdate({ name }, { born: args.setBornTo })
+      return Author.findOne({ name })
+    },
+    createUser: async (root, args) => {
+      const user = new User({ ...args })
+
+      return user.save().catch((error) => {
+        throw new GraphQLError('createing new user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+            error
+          }
+        })
+      })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if (!user || args.password !== 'secret') {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const userForToken = {
+        user: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    }
+  }
+}
+
+module.exports = resolvers
---
+++ part8/library-backend/schema.js
@@ -0,0 +1,61 @@
+const typeDefs = `
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+  type Book {
+    title: String!
+    author: Author!
+    published: Int!
+    genres: [String!]
+    id: ID!
+  }
+
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int
+    id: ID!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(
+      author: String
+      genre: String
+    ): [Book!]
+    allAuthors: [Author!]
+    me: User,
+    allGenres: [String!]
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+  }
+`
+
+module.exports = typeDefs
